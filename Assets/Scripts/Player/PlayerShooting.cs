b0a1567f7de7ad16c19efd9cd26b5869096fe7ff
David <davidmeza@outlook.com>
2015-09-25 16:10:44 -0400

    finish weapon shooting and harming enemies

--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -63,22 +63,28 @@ public class PlayerShooting : MonoBehaviour
         gunParticles.Play ();
 
         gunLine.enabled = true;
+		// The beginning of the line of the shooting ray
         gunLine.SetPosition (0, transform.position);
 
         shootRay.origin = transform.position;
         shootRay.direction = transform.forward;
 
+		// If the ray hits something
         if(Physics.Raycast (shootRay, out shootHit, range, shootableMask))
         {
+			// Give me the enemy's health script and store it in this variable
             EnemyHealth enemyHealth = shootHit.collider.GetComponent <EnemyHealth> ();
+			// If it isn't null (we didn't hit a lego block, or a wall, etc which don't have a health script)
             if(enemyHealth != null)
             {
                 enemyHealth.TakeDamage (damagePerShot, shootHit.point);
             }
+			// The ending of the line of the ray (will stop at the point where it hit an object)
             gunLine.SetPosition (1, shootHit.point);
         }
         else
         {
+			// If we didn't hit anything, then the end of the line extendends until a decided range
             gunLine.SetPosition (1, shootRay.origin + shootRay.direction * range);
         }
     }
